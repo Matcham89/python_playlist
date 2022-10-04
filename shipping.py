49be9d635d24b54ee48980505c47bc2ed8d32f62
Matcham <cjmatcham@hotmail.co.uk>
2022-10-04 14:27:59 +0100

    shipping list

--- shipping.py
+++ shipping.py
@@ -1,6 +1,6 @@
 
-
-print("Please enter the weightof your package...")
+print( )
+print("Please enter the weight of your package...")
 weight = input()
 
 #flat rates
@@ -21,31 +21,37 @@ cost_7 = int(weight)*12.00
 cost_8 = int(weight)*14.25
 
 #ground shipping flat rate + price per pound
-option_1 = int(ground_shipping) + int(cost_1)
-option_2 = int(ground_shipping) + int(cost_2)
-option_3 = int(ground_shipping) + int(cost_3)
-option_4 = int(ground_shipping) + int(cost_4)
+option_1 = int(ground_shipping) + float(cost_1)
+option_2 = int(ground_shipping) + float(cost_2)
+option_3 = int(ground_shipping) + float(cost_3)
+option_4 = int(ground_shipping) + float(cost_4)
 
 #drone flat rate + price per pound
-option_5 = int(drone_shipping) + int(cost_5)
-option_6 = int(drone_shipping) + int(cost_6)
-option_7 = int(drone_shipping) + int(cost_7)
-option_8 = int(drone_shipping) + int(cost_8)
+option_5 = int(drone_shipping) + float(cost_5)
+option_6 = int(drone_shipping) + float(cost_6)
+option_7 = int(drone_shipping) + float(cost_7)
+option_8 = int(drone_shipping) + float(cost_8)
 
-print("Ground Shippint")
-print("under 2lb £" + str(option_1)) 
-print("Over 2 lb but less than or equal to 6 lb £" + str(option_2))
-print( "Over 6 lb but less than or equal to 10 lb £" + str(option_3))
-print("Over 10 lb £" + str(option_4)) 
+print("You entered" + weight + "lb")
+print(" ")
+print("Ground Shipping")
+print("Option 1 - Under 2lb " + "=" + " £" + str(option_1)) 
+print("Option 2 - Over 2lb but less than or equal to 6lb " + "=" + " £" + str(option_2))
+print("Option 3 - Over 6lb but less than or equal to 10lb " + "=" + " £" + str(option_3))
+print("Option 4 - Over 10lb " + "=" + " £" + str(option_4)) 
 print(" ")
 print("Drone Shipping")
-print("under 2lb £" + str(option_5))
-print("Over 2 lb but less than or equal to 6 lb £" + str(option_6)) 
-print("Over 6 lb but less than or equal to 10 lb £" + str(option_7)) 
-print("Over 10 lb £" + str(option_8))
+print("Option 5 - Under 2lb " + "=" + " £" + str(option_5))
+print("Option 6 - Over 2lb but less than or equal to 6lb " + "=" + " £" + str(option_6)) 
+print("Option 7 - Over 6lb but less than or equal to 10lb " + "=" + " £" + str(option_7)) 
+print("Option 8 - Over 10lb " +  "=" + " £" + str(option_8))
 print(" ")
-print("Given the weight of your package at " + str(weight) + "lb")
-
+print("Premium Shipping")
+print("£125")
+print(" ")
+print(" ")
+print("Given the weight of your package at " + str(weight) + "lb" + " and the cost of each shipping option.")
+print(" ")
 option = "The cheapest option for shipment is "
 grnd = "*Ground Shipping* "
 prem = "*Premium Shipping* "
@@ -89,4 +95,5 @@ elif option_8 > option_4 and int(weight) > 10 and option_8 < premium_shipping:
     print(option + grnd + "at the cost of £" + str(option_4) + " option 4")
 elif option_8 > option_4 and int(weight) > 10 and option_7 > premium_shipping:
     print(option + prem + "at the cost of £" + str(premium_shipping) + " premium shipping")
-
+print(" ")
+print(" ")
